bef5a936d8987879d5c326e5d28156c70b871f3c
Bhagwan Thapa <bhagwanthapawork@gmail.com>
2022-11-15 20:06:35 +0545

    feat: add db connection

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 import os
 import pathlib
+from dotenv import load_dotenv
 
 BASE_DIR = pathlib.Path(__file__).parent.resolve()
 CORE_DIR = os.path.join(BASE_DIR, 'core')
 INPUT_CSV_PATH = os.path.join(BASE_DIR, 'data', 'input_csv')
 OUTPUT_CSV_PATH = os.path.join(BASE_DIR, 'data', 'output_csv')
 META_PATH = os.path.join(BASE_DIR, 'data', 'meta')
+
+
+load_dotenv()
+
+DB_DRIVER = os.getenv('DB_DRIVER')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
---
+++ db.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+import config
+
+
+def get_connection() -> pyodbc.Connection:
+    driver_str = f'DRIVER={{{config.DB_DRIVER}}}'
+    server_str = f'SERVER={config.DB_HOST}'
+    database_str = f'DATABASE={config.DB_NAME}'
+    user_str = f'UID={config.DB_USER}'
+    pwd_str = f'PWD={config.DB_PASSWORD}'
+    cert_str = 'TrustServerCertificate=yes'
+
+    connection_str = ";".join([driver_str, server_str, database_str, user_str, pwd_str, cert_str])
+    connection = pyodbc.connect(connection_str)
+    return connection
